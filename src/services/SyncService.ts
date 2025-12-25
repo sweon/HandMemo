29f3b25534a8fb2517edbc170120441283458d96
Seyeon Weon <seyeon.weon@gmail.com>
2025-12-25 21:24:56 +0900

    fix(sync): optimize ICE config for same-WiFi connectivity

--- src/services/SyncService.ts
+++ src/services/SyncService.ts
@@ -53,8 +53,12 @@ export class SyncService {
                         { urls: 'stun:stun.l.google.com:19302' },
                         { urls: 'stun:stun1.l.google.com:19302' },
                         { urls: 'stun:stun2.l.google.com:19302' },
+                        { urls: 'stun:stun3.l.google.com:19302' },
+                        { urls: 'stun:stun4.l.google.com:19302' },
+                        { urls: 'stun:stun.services.mozilla.com' },
                         { urls: 'stun:global.stun.twilio.com:3478' }
-                    ]
+                    ],
+                    iceCandidatePoolSize: 10
                 }
             };
 
