7100226b349763d7e6255c65ea0e0f70ff02bcdb
Seyeon Weon <seyeon.weon@gmail.com>
2026-01-16 07:06:42 +0900

    feat: apply rounded corners (squircle) to app icons with transparent background

--- vite.config.ts
+++ vite.config.ts
@@ -23,13 +23,25 @@ export default defineConfig({
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       },
